d415764da64d065ae45e1ba172980706d7c8340a
Villera69 <william.ramsfeldt@gmail.com>
2023-05-15 10:31:45 +0200

    First Commit

---
+++ src/NetworkingClient.java
@@ -0,0 +1,53 @@
+/* CLIENT */
+import java.io.*;
+import java.net.*;
+
+public class NetworkingClient {
+
+    public static void main(String args[]) {
+
+        Socket client = null;
+
+        int portnumber = 1234;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        for (int i=0; i <10; i++) {
+            try {
+                String msg = "";
+
+                client = new Socket(InetAddress.getLocalHost(), portnumber);
+                System.out.println("Client socket is created " + client);
+
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut, true);
+
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new
+                        InputStreamReader(clientIn));
+
+                BufferedReader stdIn = new BufferedReader(new
+                        InputStreamReader(System.in));
+
+                System.out.println("Enter your name. Type Bye to exit. ");
+
+                msg = stdIn.readLine().trim();
+                pw.println(msg);
+
+                System.out.println("Message returned from the server = " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                if (msg.equalsIgnoreCase("Bye")) {
+                    break;
+                }
+
+            } catch (IOException ie) {
+                System.out.println("I/O error " + ie);
+            }
+        }
+    }
+}
